256c4822eda37fafeeaa49b8f03f5259489ac7b8
James Rovel Barbosa <diagjames@gmail.com>
2022-02-20 21:08:46 -0300

    working

--- app/src/main/java/com/example/app2multiactivity/MainActivity.java
+++ app/src/main/java/com/example/app2multiactivity/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +17,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
-        Intent it = new Intent(this, Resultado.class);
-        startActivity(it);
+        EditText inputName = findViewById(R.id.editTextName);
+        EditText frequence = findViewById(R.id.editTextFrequence);
+        EditText nota1 = findViewById(R.id.editTextNota1);
+        EditText nota2 = findViewById(R.id.editTextNota2);
+
+        if (inputName.length() == 0 || frequence.length() == 0) {
+            Toast msg = Toast.makeText(this, "Insira um valor no campo", Toast.LENGTH_SHORT);
+            msg.show();
+        } else {
+            Intent it = new Intent(this, Resultado.class);
+            String name = inputName.getText().toString();
+            int not1 = Integer.parseInt(nota1.getText().toString());
+            int not2 = Integer.parseInt(nota2.getText().toString());
+            int MediaFinal = (not1 + not2) / 2;
+            String mf = Integer.toString(MediaFinal);
+            String freq = frequence.getText().toString();
+
+            Bundle params = new Bundle();
+            params.putString("name_key", name);
+            params.putString("mf_key", mf);
+            params.putString("freq_key", freq);
+
+            it.putExtras(params);
+            startActivity(it);
+        }
     }
 }
--- app/src/main/java/com/example/app2multiactivity/Resultado.java
+++ app/src/main/java/com/example/app2multiactivity/Resultado.java
@@ -2,7 +2,9 @@ package com.example.app2multiactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Resultado extends AppCompatActivity {
 
@@ -10,6 +12,41 @@ public class Resultado extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+        TextView outName = findViewById(R.id.textViewOutName);
+        TextView outMedia = findViewById(R.id.textViewMedia);
+        TextView outFreq = findViewById(R.id.textViewFreq);
+        TextView resultado = findViewById(R.id.textViewResult);
+
+        Intent it = getIntent();
+
+        if (it != null) {
+            Bundle params = it.getExtras();
+
+            if (params != null) {
+                String result = params.getString("name_key");
+                String media = params.getString("mf_key");
+                String freq = params.getString("freq_key");
+                outName.setText("Bem vindo " + result);
+                outMedia.setText(media);
+                outFreq.setText(freq);
+
+                int mf = Integer.parseInt(media);
+                int frequence = Integer.parseInt(freq);
+
+                if (mf >= 70 && frequence >= 75 ) {
+                    resultado.setText("Parabéns, você foi aprovado!");
+                } else if (mf >= 40 && mf <= 69 && frequence >= 75) {
+                    resultado.setText("Você ficou para a final!");
+                } else if (frequence < 75) {
+                    resultado.setText("Você foi reprovado por falta!");
+                } else if (mf < 40) {
+                    resultado.setText("Você foi reprovado por nota!");
+                }
+
+            }
+
+
+        }
     }
 
 
